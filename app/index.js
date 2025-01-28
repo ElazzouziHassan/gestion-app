2629152f00f0ce239f117a3cec820613ed3f051a
Wizardy <wizardhassan99@gmail.com>
2025-01-28 14:48:28 +0100

    home page : this home page still in developemnt mode. will be edited later on

--- app/index.js
+++ app/index.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
-const index = () => {
+const Home = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" hidden={false} />
+      <Text style={styles.header}>This is home Page</Text>
+      <Link href="/auth/login" style={styles.link}>Go to login</Link>
+      <Link href="/auth/forgot-password" style={styles.link}>Go to Forgot Password</Link>
+      <Link href="/auth/new-password" style={styles.link}>Go to Set Up New Password</Link>
+      <Link href="/auth/alert" style={styles.link}>alert</Link>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  link: {
+    fontSize: 18,
+    color: '#1e90ff',
+    marginVertical: 10,
+  },
+});
