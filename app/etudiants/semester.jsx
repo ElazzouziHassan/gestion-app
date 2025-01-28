ea9f955abc709c6a5f451a6e2f979383be84d210
Wizardy <wizardhassan99@gmail.com>
2025-01-28 19:15:14 +0100

    commit

---
+++ app/etudiants/semester.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Semester = () => {
+  return (
+    <View>
+      <Text>Semester</Text>
+    </View>
+  )
+}
+
+export default Semester
+
+const styles = StyleSheet.create({})
