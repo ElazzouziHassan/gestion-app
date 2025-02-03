210ad74408a97b42e4ea3d87f9d2024ff9bb2a86
Wizardy <wizardhassan99@gmail.com>
2025-02-03 20:28:55 +0100

    commit

---
+++ app/students/schedules.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Emploi = () => {
+  return (
+    <View>
+      <Text>Emploi</Text>
+    </View>
+  )
+}
+
+export default Emploi
+
+const styles = StyleSheet.create({})
